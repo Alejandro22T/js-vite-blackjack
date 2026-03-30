5001579bb160a427d952f9d57713c016cba378f7
Alejandro Espinal <alejojet22@gmail.com>
2026-03-30 12:52:30 -0600

    firts commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+/**
+ * Esta función me permite tomar una carta
+ * @param {Array<String>} deck es una arreglo de string
+ * @returns {String} retorna una carta aleatoria del deck
+ */
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    
+    return carta;
+}
---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,14 @@
+/**
+ * Función para obtener el valor de la carta 
+ * @param {String} carta reciba una carta del deck 
+ * @returns {Number} retorna el valor de la carta
+ */
+export const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    console.log(valor);
+    
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
